d05b5d5ee6decbe0db321e79a6ad979d25b86aa1
jjjimenez340-wq <jjjimenez340@gmail.com>
2026-01-31 18:11:07 +0000

    my-green-red-dog.js

---
+++ my-dog.js
@@ -0,0 +1,15 @@
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0, 50, 200);
+  fill("red")
+  ellipse(220, 200, 90, 40);
+  fill("green")
+circle(185, 180, 43);
+
+
+
+}
